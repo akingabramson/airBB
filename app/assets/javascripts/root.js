54fe538dd25a63a6d1df3da47238fab937feac03
Asher King Abramson <a.kingabramson@gmail.com>
2013-07-23 14:15:12 -0700

    should have done this before devise stuff.  oh well

---
+++ app/assets/javascripts/root.js
@@ -0,0 +1,66 @@
+var map;
+
+function initialize() {
+  var mapOptions = {
+    zoom: 6,
+    mapTypeId: google.maps.MapTypeId.ROADMAP
+  };
+  map = new google.maps.Map(document.getElementById('map-canvas'),
+      mapOptions);
+
+  // Try HTML5 geolocation
+  if(navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(function(position) {
+      var pos = new google.maps.LatLng(position.coords.latitude,
+                                       position.coords.longitude);
+-
+      var infowindow = new google.maps.InfoWindow({
+        map: map,
+        position: pos,
+        content: 'Location found using HTML5.'
+      });
+
+      map.setCenter(pos);
+    }, function() {
+      handleNoGeolocation(true);
+    });
+    
+
+  } else {
+    // Browser doesn't support Geolocation
+    handleNoGeolocation(false);
+  }
+
+  loadMarkers();
+
+}
+
+function loadMarkers() {
+  console.log("here")
+  var newPos = new google.maps.LatLng(37.7811588, -122.41146719999999);
+  // load different image based on size?
+  var marker = new google.maps.Marker({
+    position: newPos,
+    map: map,
+    title: "Hello World!"
+  })
+}
+
+function handleNoGeolocation(errorFlag) {
+  if (errorFlag) {
+    var content = 'Error: The Geolocation service failed.';
+  } else {
+    var content = 'Error: Your browser doesn\'t support geolocation.';
+  }
+
+  var options = {
+    map: map,
+    position: new google.maps.LatLng(60, 105),
+    content: content
+  };
+
+  var infowindow = new google.maps.InfoWindow(options);
+  map.setCenter(options.position);
+}
+
+google.maps.event.addDomListener(window, 'load', initialize);
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AirBB::Application.config.secret_token = 'abaf5c3baf6b76bb00b73fa2912f2ebf361e5c919031ef778c98d4e72742735f4b5aa821db8696f5e46613b4e8b265333a103af338b7b2e70d783d93992a08bd'
