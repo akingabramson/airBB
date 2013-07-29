cc6e1499533335b8d69a1d81bb5433cdcd336885
Asher King Abramson <a.kingabramson@gmail.com>
2013-07-28 20:16:19 -0700

    form toggling works

--- app/assets/javascripts/views/courts/new_court.js
+++ app/assets/javascripts/views/courts/new_court.js
@@ -1,5 +1,9 @@
 AB.Views.NewCourt = Backbone.View.extend({
 	template: JST["courts/new"],
+	initialize: function() {
+		this.$el.attr("id", "new-court-form");
+		this.$el.addClass("hidden");
+	},
 	render: function() {
 		var renderedForm = this.template()
 		this.$el.html(renderedForm)
--- app/assets/javascripts/views/maps/maps_index.js
+++ app/assets/javascripts/views/maps/maps_index.js
@@ -1,11 +1,18 @@
 AB.Views.Main = Backbone.View.extend({
   template: JST['maps/index'],
+  events: {
+  	"click #new-court-button": "showNewCourtForm",
+  },
+
   render: function() {
   	var renderedMapContent = this.template();
   	this.$el.html(renderedMapContent);
-  	var courtFormView = new AB.Views.NewCourt();
-  	this.$el.prepend(courtFormView.render().$el);
+  	this.courtFormView = new AB.Views.NewCourt();
+  	this.$el.prepend(this.courtFormView.render().$el);
   	return this;
   },
 
+  showNewCourtForm: function() {
+  	this.courtFormView.$el.toggleClass("hidden");
+  }
 });
