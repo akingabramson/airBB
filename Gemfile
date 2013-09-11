ed46a4228eb02c9167d3ca9070f3a54e2da0fda7
Asher King Abramson <a.kingabramson@gmail.com>
2013-09-11 11:12:02 -0700

    configured postgres

--- Gemfile
+++ Gemfile
@@ -2,20 +2,9 @@ source 'https://rubygems.org'
 
 gem 'rails', '3.2.11'
 
-# Bundle edge Rails instead:
-# gem 'rails', :git => 'git://github.com/rails/rails.git'
-
-
-
-# Gems used only for assets and not required
-# in production environments by default.
 group :assets do
   gem 'sass-rails',   '~> 3.2.3'
   gem 'coffee-rails', '~> 3.2.1'
-
-  # See https://github.com/sstephenson/execjs#readme for more supported runtimes
-  # gem 'therubyracer', :platforms => :ruby
-
   gem 'uglifier', '>= 1.0.3'
 end
 
@@ -28,10 +17,11 @@ group :development do
   gem 'whenever', :require => false
 end
 
+gem "pg"
+
 gem "omniauth"
 gem "omniauth-facebook"
 gem "devise"
-gem "sqlite3"
 gem 'jquery-rails'
 gem 'underscore-rails'
 gem 'backbone-on-rails'
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130911170504) do
+ActiveRecord::Schema.define(:version => 20130909203340) do
 
   create_table "check_ins", :force => true do |t|
     t.integer  "user_id"
@@ -31,22 +31,6 @@ ActiveRecord::Schema.define(:version => 20130911170504) do
 
   add_index "courts", ["latitude"], :name => "index_courts_on_latitude"
   add_index "courts", ["longitude"], :name => "index_courts_on_longitude"
-
-  create_table "delayed_jobs", :force => true do |t|
-    t.integer  "priority",   :default => 0, :null => false
-    t.integer  "attempts",   :default => 0, :null => false
-    t.text     "handler",                   :null => false
-    t.text     "last_error"
-    t.datetime "run_at"
-    t.datetime "locked_at"
-    t.datetime "failed_at"
-    t.string   "locked_by"
-    t.string   "queue"
-    t.datetime "created_at",                :null => false
-    t.datetime "updated_at",                :null => false
-  end
-
-  add_index "delayed_jobs", ["priority", "run_at"], :name => "delayed_jobs_priority"
 
   create_table "users", :force => true do |t|
     t.string   "email",                  :default => "", :null => false
